00bb1f6c1fc8a8be6fdbee3dd4990de3f481d5cb
Wesley <wesleyrg2000@gmail.com>
2018-03-01 14:28:46 -0400

    Initial commit

---
+++ total_learning/lib/total_learning.ex
@@ -0,0 +1,18 @@
+defmodule TotalLearning do
+  @moduledoc """
+  Documentation for TotalLearning.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TotalLearning.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ total_learning/test/total_learning_test.exs
@@ -0,0 +1,8 @@
+defmodule TotalLearningTest do
+  use ExUnit.Case
+  doctest TotalLearning
+
+  test "greets the world" do
+    assert TotalLearning.hello() == :world
+  end
+end
