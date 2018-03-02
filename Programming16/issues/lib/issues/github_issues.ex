e9ac62d274e7d0f9f648f8a1226421b4084d5dae
Wesley <wesleyrg2000@gmail.com>
2018-03-02 16:01:10 -0400

    02.03.18.16:01

--- Programming16/issues/lib/issues/github_issues.ex
+++ Programming16/issues/lib/issues/github_issues.ex
@@ -20,6 +20,7 @@ defmodule Issues.GithubIssues do
   def handle_response({_, %{status_code: status_code, body: body}}) do
     Logger.info("Got response: status code=#{status_code}")
     Logger.debug(fn -> inspect(body) end)
+    # require IEx; IEx.pry
     {
       status_code |> check_for_error(),
       body |> Poison.Parser.parse!()
--- total_learning/lib/pro16/htt_poison.ex
+++ total_learning/lib/pro16/htt_poison.ex
@@ -1,3 +1,12 @@
 defmodule Pro16.HttPoison do
-  
+  @moduledoc "Esse módulo é apenas um test para HttPoison"
+
+  @doc "Essa função faz um request do site com dados passsados"
+  def request_http(user \\ "Weryrg3", project \\ "Projects") do
+    HTTPoison.get("https://github.com/#{user}/#{project}")
+    |> htt_body()
+  end
+
+  defp htt_body({:ok, %{status_code: 200, body: body}}), do: {:ok, Poison.decode(body)}
+  defp htt_body({_, %{status_code: _, body: body}}), do: {:error, Poison.decode(body)}
 end
