b02fdfab3c9d699f98e99a45bc0c059e3b3ece7c
Wesley <wesleyrg2000@gmail.com>
2018-03-05 11:28:53 -0400

    Exclusão de arquivos

--- Programming16/concurrent_programming/test/concurrent_programming_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ConcurrentProgrammingTest do
-  use ExUnit.Case
-  doctest ConcurrentProgramming
-
-  test "greets the world" do
-    assert ConcurrentProgramming.hello() == :world
-  end
-end
