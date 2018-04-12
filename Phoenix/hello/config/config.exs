1b9d4c78878c67b97c9c0ff45f073d4fb0229233
Wesley <wesleyrg2000@gmail.com>
2018-04-12 09:56:45 -0400

    phoenix

--- Phoenix/hello/config/config.exs
+++ Phoenix/hello/config/config.exs
@@ -1,27 +1,15 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# General application configuration
-config :hello,
-  ecto_repos: [Hello.Repo]
+config :hello, ecto_repos: [Hello.Repo]
 
-# Configures the endpoint
 config :hello, Hello.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "lEGYQZWXmUyPeJ7UXYUOp7MoXf7Uwwr44+5i+yt80ZX2EK0li4AXpsWnJSKhiPFo",
   render_errors: [view: Hello.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Hello.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Hello.PubSub, adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
