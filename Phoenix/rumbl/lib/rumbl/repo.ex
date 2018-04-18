bca525cdd672f68b3903709829aaec90b943de44
Wesley <wesleyrg2000@gmail.com>
2018-04-18 10:00:42 -0400

    .call

--- Phoenix/rumbl/lib/rumbl/repo.ex
+++ Phoenix/rumbl/lib/rumbl/repo.ex
@@ -1,28 +1,6 @@
 defmodule Rumbl.Repo do
-  alias Rumbl.User
-  # use Ecto.Repo, otp_app: :rumbl
+  use Ecto.Repo, otp_app: :rumbl
   @moduledoc """
     In memory repository.
   """
-  def all(Rumbl.User) do
-    [
-      %User{id: "1", name: "José", username: "josevalim", password: "elixir"},
-      %User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-      %User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find(all(module), fn map -> map.id == id end)
-  end
-
-  def get_by(module, params) do
-    Enum.find(all(module), fn map ->
-      Enum.all?(params, fn {key, val} ->
-        Map.get(map, key) == val
-      end)
-    end)
-  end
 end
