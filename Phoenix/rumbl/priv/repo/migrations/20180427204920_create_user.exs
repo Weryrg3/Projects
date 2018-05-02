8c14f9f22aa9cd8cc7de43c2546ccea26d727a36
Wesley <wesleyrg2000@gmail.com>
2018-05-02 11:19:18 -0400

    novo

--- Phoenix/rumbl/priv/repo/migrations/20180427204920_create_user.exs
+++ Phoenix/rumbl/priv/repo/migrations/20180427204920_create_user.exs
@@ -5,7 +5,7 @@ defmodule Rumbl.Repo.Migrations.CreateUser do
     create table(:users) do
       add(:name, :string)
       add(:username, :string, null: false)
-      add(:password_hase, :string)
+      add(:password_hash, :string)
 
       timestamps()
     end
