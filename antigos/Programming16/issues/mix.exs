03cc3aeb9bf8d02e88571735de0d1dd4d6f7c681
Wesley <wesleyrg2000@gmail.com>
2018-05-18 09:06:56 -0400

    new

---
+++ antigos/Programming16/issues/mix.exs
@@ -0,0 +1,41 @@
+defmodule Issues.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :issues,
+      escript: escript_config(),
+      version: "0.1.0",
+      elixir: "~> 1.6-rc",
+      start_permanent: Mix.env() == :prod,
+      name: "Issues",
+      source_url: "https://github.com/pragdave/issues",
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [extra_applications: [:logger] ]
+    # [applications: [:logger, :httpoison] ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 1.0.0"},
+      {:poison, "~> 3.1"},
+      {:ex_doc, "~> 0.18.1"},
+      {:earmark, "~> 1.2.4"},
+      {:hackney, "~> 1.11"},
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
+    ]
+  end
+end
