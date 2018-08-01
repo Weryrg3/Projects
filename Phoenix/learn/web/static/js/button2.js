9973db5c6390dea4f6e26851dfb1ce7c47d84119
Wesley <wesleyrg2000@gmail.com>
2018-08-01 09:10:37 -0400

    qua ago  1 09:10:36 -04 2018 -- Commit by script in gnome-terminal

--- Phoenix/learn/web/static/js/button2.js
+++ Phoenix/learn/web/static/js/button2.js
@@ -5,9 +5,18 @@ let Button = {
         this.onReady("buttons2", socket)
     },
     onReady(buttonId, socket) {
+        let div_vazia = document.getElementById("div_vazia")
+        let div_buttons2 = document.getElementById("div_buttons2")
         let buttonChannel = socket.channel("buttons2:" + buttonId)
+
+        div_buttons2.addEventListener("click", e => {
+            let params = {params: params} 
+        }
+
+    )
+
         buttonChannel.join()
-            .receive("ok", resp => console.log("Buttons unido ao Channel", resp))
+            .receive("ok", resp => console.log("Iniciando Buttons2 Channel", resp))
             .receive("error", reason => console.log("Falha", reason))
     }
 }
